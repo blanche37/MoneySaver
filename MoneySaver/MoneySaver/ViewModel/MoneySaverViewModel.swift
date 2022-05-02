6530c53af7ca94910297a04954a122c8053d6235
blanche37 <wkddbsghk16@gmail.com>
2022-05-02 21:24:23 +0900

    feat: ViewModel protocol 구현

--- MoneySaver/MoneySaver/ViewModel/MoneySaverViewModel.swift
+++ MoneySaver/MoneySaver/ViewModel/MoneySaverViewModel.swift
@@ -6,9 +6,15 @@
 //
 
 import Foundation
+import CoreData
 
 protocol ViewModel {
+    var challenges: Observable<[NSManagedObject]> { get }
+    var consumptions: Observable<[NSManagedObject]> { get }
     
+    func add(item: NSManagedObject)
+    func fetchChallenges()
+    func fetchConsumptions()
 }
 
 final class MoneySaverViewModel: ViewModel {
