5a00a10ed58ab36e6736481f1d23a5dbc15a43e6
blanche37 <wkddbsghk16@gmail.com>
2022-05-02 18:52:05 +0900

    feat: CoreDataStack 구현
    persistentContainer 구현

--- MoneySaver/MoneySaver/CoreData/CoreDataStack.swift
+++ MoneySaver/MoneySaver/CoreData/CoreDataStack.swift
@@ -6,7 +6,26 @@
 //
 
 import Foundation
+import CoreData
 
 class CoreDataStack {
+    static let shared = CoreDataStack()
     
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Person")
+        let description = NSPersistentStoreDescription()
+        description.type = NSSQLiteStoreType
+        container.persistentStoreDescriptions = [description]
+        
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unable to load core data persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
 }
