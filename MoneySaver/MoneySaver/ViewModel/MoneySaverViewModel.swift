a7da54929bb80baffbd9d4fe90de36a01ed73d8f
blanche37 <wkddbsghk16@gmail.com>
2022-05-03 14:18:04 +0900

    feat: MoneySaverViewModel 메서드 구현
    add(), fetchConsumption(), removeAll() 구현

--- MoneySaver/MoneySaver/ViewModel/MoneySaverViewModel.swift
+++ MoneySaver/MoneySaver/ViewModel/MoneySaverViewModel.swift
@@ -18,5 +18,51 @@ protocol ViewModel {
 }
 
 final class MoneySaverViewModel: ViewModel {
+    var service: Service!
     
+    var challenges = Observable<[NSManagedObject]>([])
+    var consumptions = Observable<[NSManagedObject]>([])
+    
+    func add(item: NSManagedObject) {
+        if item is Challenge {
+            challenges.value.append(item)
+        } else {
+            consumptions.value.append(item)
+        }
+    }
+    
+    func fetchChallenges() {
+        service.fetchChallenges { [weak self] challenges in
+            guard let self = self else {
+                return
+            }
+            
+            self.challenges.value = challenges
+        }
+    }
+    
+    func fetchConsumptions() {
+        service.fetchConsumptions { [weak self] consumptions in
+            guard let self = self else {
+                return
+            }
+            
+            self.consumptions.value = consumptions
+        }
+    }
+    
+    func removeAll() {
+        service.removeAll { [weak self] in
+            guard let self = self else {
+                return
+            }
+            
+            self.challenges.value = []
+            self.consumptions.value = []
+        }
+    }
+    
+    init(service: Service) {
+        self.service = service
+    }
 }
