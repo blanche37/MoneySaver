2c7ece35b7e6934daad9e2db161e4240c004b1b4
blanche37 <wkddbsghk16@gmail.com>
2022-05-09 19:09:58 +0900

    feat: middleView 폴드기능 추가

--- MoneySaver/MoneySaver/View/DetailViewController.swift
+++ MoneySaver/MoneySaver/View/DetailViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 class DetailViewController: UIViewController {
     // MARK: - Properties
     var viewModel: ViewModel!
+    @IBOutlet weak var middleView: InformationView!
+    @IBOutlet weak var bottomView: InformationView!
+    @IBOutlet weak var middleHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.layer.cornerRadius = 13
+        let deviceHeight = UIScreen.main.bounds.height
+        middleHeightConstraint.constant = deviceHeight / 4 * 3
+        bottomHeightConstraint.constant = middleView.bounds.height / 4 * 3
+        bottomView.isHidden = true
+        
     }
     
     // MARK: - IBActions
@@ -29,6 +39,42 @@ class DetailViewController: UIViewController {
         childVC.title = "Update"
         childVC.viewModel = viewModel
         self.present(vc, animated: true)
+    }
+    
+    @IBAction func foldMiddleView(_ sender: UIButton) {
+        let deviceHeight = UIScreen.main.bounds.height
+        
+        if middleHeightConstraint.constant == 0 {
+            bottomHeightConstraint.constant = middleView.bounds.height / 4 * 3
+            middleHeightConstraint.constant = deviceHeight / 4 * 3
+            
+            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) { [weak self] in
+                guard let self = self else {
+                    return
+                }
+                
+                self.view.layoutIfNeeded()
+            } completion: { [weak self] _ in
+                guard let self = self else {
+                    return
+                }
+                
+                self.bottomView.isHidden = true
+            }
+
+        } else {
+            bottomView.isHidden = false
+            middleHeightConstraint.constant = 0
+            bottomHeightConstraint.constant = 0
+            
+            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) { [weak self] in
+                guard let self = self else {
+                    return
+                }
+                
+                self.view.layoutIfNeeded()
+            }
+        }
     }
 }
 
