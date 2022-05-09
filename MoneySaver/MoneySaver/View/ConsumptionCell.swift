45c8ad50479e6a9fe32a291c7723f61ea3f36821
blanche37 <wkddbsghk16@gmail.com>
2022-05-09 17:12:55 +0900

    feat: ConsumptionCell bind()메서드 구현

--- MoneySaver/MoneySaver/View/ConsumptionCell.swift
+++ MoneySaver/MoneySaver/View/ConsumptionCell.swift
@@ -8,5 +8,17 @@
 import UIKit
 
 class ConsumptionCell: UITableViewCell {
-    
+    @IBOutlet private weak var itemLabel: UILabel!
+    @IBOutlet private weak var priceLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+}
+
+extension ConsumptionCell: NumberFormattable, DateFormattable {
+    func bind(item: String, price: Double, date: Date, currency: String) {
+        let convertedPrice = changeNumberFormat(currency: currency, number: price)
+        let convertedDate = changedDateFormat(date: date)
+        itemLabel.text = item
+        priceLabel.text = convertedPrice
+        dateLabel.text = convertedDate
+    }
 }
