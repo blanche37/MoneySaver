b96ff8636634e5a2acffb8e44c385447320b164a
blanche37 <wkddbsghk16@gmail.com>
2022-05-09 12:08:52 +0900

    feat: barButtonItem추가후 IBAction연결

--- MoneySaver/MoneySaver/View/DetailViewController.swift
+++ MoneySaver/MoneySaver/View/DetailViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    // MARK: - Properties
     var viewModel: ViewModel!
     
+    // MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - IBActions
+    @IBAction func updateChallenge(_ sender: UIBarButtonItem) {
+    }
+    
 }
